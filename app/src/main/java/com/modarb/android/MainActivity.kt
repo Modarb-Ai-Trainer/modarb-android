04ff0d193d152d3c27ab3c01f8b5bddc0b99f99f
Amr Hossam <amrhossam.dev@gmail.com>
2023-12-22 19:57:42 +0200

    i'm still baby and this is my first commit.

---
+++ app/src/main/java/com/modarb/android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.modarb.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
