465242e486728b8197d7049eced7fec2cd1097c2
Amr Hossam <amrhossam.dev@gmail.com>
2023-12-24 00:07:54 +0200

    Fixed WelcomeScreenActivity

---
+++ app/src/main/java/com/modarb/android/ui/onboarding/activities/WelcomeScreenActivity.kt
@@ -0,0 +1,12 @@
+    package com.modarb.android.ui.onboarding.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.modarb.android.R
+
+class WelcomeScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_screen)
+    }
+}
