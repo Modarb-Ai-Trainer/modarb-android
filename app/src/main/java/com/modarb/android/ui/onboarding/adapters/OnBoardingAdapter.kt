b55644a6112607f6ea09fcd80107a10eaacec4d3
Amr Hossam <amrhossam.dev@gmail.com>
2023-12-31 00:40:46 +0200

    Refactor

--- app/src/main/java/com/modarb/android/ui/onboarding/adapters/OnBoardingAdapter.kt
+++ app/src/main/java/com/modarb/android/ui/onboarding/adapters/OnBoardingAdapter.kt
@@ -14,7 +14,7 @@ import com.modarb.android.ui.onboarding.ViewPagerViews.SelectFitnessLevelView
 import com.modarb.android.ui.onboarding.ViewPagerViews.SelectGenderView
 import com.modarb.android.ui.onboarding.ViewPagerViews.SelectGoalView
 import com.modarb.android.ui.onboarding.ViewPagerViews.SelectTargetWeightView
-import com.modarb.android.ui.onboarding.models.ItemSelectionModel
+import com.modarb.android.ui.onboarding.utils.Data
 
 class OnBoardingAdapter(private val views: List<View>, val ctx: Context) :
     RecyclerView.Adapter<OnBoardingAdapter.ViewHolder>() {
@@ -59,7 +59,7 @@ class OnBoardingAdapter(private val views: List<View>, val ctx: Context) :
                 }
 
                 3 -> {
-                    MessageView(view, ctx, getBMIData())
+                    MessageView(view, ctx, Data.getBMIData())
                 }
 
                 4 -> {
@@ -72,43 +72,16 @@ class OnBoardingAdapter(private val views: List<View>, val ctx: Context) :
 
                 6 -> {
                     ItemSelectionView(
-                        view, ctx, getEquipmentList(), R.string.what_equipment_do_you_have
+                        view, ctx, Data.getEquipmentList(), R.string.what_equipment_do_you_have
                     )
                 }
 
                 7 -> {
-                    ItemSelectionView(view, ctx, getPainPositions(), R.string.do_you_have_pain)
+                    ItemSelectionView(view, ctx, Data.getPainPositions(), R.string.do_you_have_pain)
                 }
             }
         }
     }
 
-    private fun getEquipmentList(): List<ItemSelectionModel> {
-        return listOf(
-            ItemSelectionModel("Barbells"),
-            ItemSelectionModel("Dumbbells"),
-            ItemSelectionModel("Gym machines"),
-            ItemSelectionModel("Resistance band"),
-            ItemSelectionModel("Body weight")
-        )
-    }
 
-    private fun getPainPositions(): List<ItemSelectionModel> {
-        return listOf(
-            ItemSelectionModel("Neek"),
-            ItemSelectionModel("Shoulders"),
-            ItemSelectionModel("Back"),
-            ItemSelectionModel("Arms"),
-            ItemSelectionModel("Knees")
-        )
-    }
-
-    private fun getBMIData(): List<String> {
-        val bmi = 22.23
-        return listOf(
-            "Great news! \uD83C\uDF89 Your BMI is ${bmi}, which falls within a healthy range. Your goal of reaching 58 kg is within reach, and we're here to support you every step of the way!",
-            "Now, let's tailor your workouts to your fitness level and preferences.",
-            "Your personalized journey awaits!"
-        )
-    }
 }
---
+++ app/src/main/java/com/modarb/android/ui/onboarding/utils/Data.kt
@@ -0,0 +1,39 @@
+package com.modarb.android.ui.onboarding.utils
+
+import com.modarb.android.ui.onboarding.models.ItemSelectionModel
+
+class Data {
+
+    companion object {
+        fun getEquipmentList(): List<ItemSelectionModel> {
+            return listOf(
+                ItemSelectionModel("Barbells"),
+                ItemSelectionModel("Dumbbells"),
+                ItemSelectionModel("Gym machines"),
+                ItemSelectionModel("Resistance band"),
+                ItemSelectionModel("Body weight")
+            )
+        }
+
+        fun getPainPositions(): List<ItemSelectionModel> {
+            return listOf(
+                ItemSelectionModel("Neek"),
+                ItemSelectionModel("Shoulders"),
+                ItemSelectionModel("Back"),
+                ItemSelectionModel("Arms"),
+                ItemSelectionModel("Knees")
+            )
+        }
+
+        fun getBMIData(): List<String> {
+            val bmi = 22.23
+            return listOf(
+                "Great news! \uD83C\uDF89 Your BMI is ${bmi}, which falls within a healthy range. Your goal of reaching 58 kg is within reach, and we're here to support you every step of the way!",
+                "Now, let's tailor your workouts to your fitness level and preferences.",
+                "Your personalized journey awaits!"
+            )
+        }
+    }
+
+
+}
