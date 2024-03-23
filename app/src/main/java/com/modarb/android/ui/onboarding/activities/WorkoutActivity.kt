8673feb83098526c060aac83e160420ebba23b5d
Basem Khater <fagreleslam514@gmail.com>
2024-03-23 02:02:35 +0200

    Adding first WorkoutActivity.

---
+++ app/src/main/java/com/modarb/android/ui/onboarding/activities/WorkoutActivity.kt
@@ -0,0 +1,41 @@
+package com.modarb.android.ui.onboarding.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.modarb.android.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.modarb.android.ui.onboarding.adapters.WorkoutAdapter
+import com.modarb.android.ui.onboarding.models.WorkoutModel
+
+class WorkoutActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var startButton: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_workout)
+
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        val itemList: ArrayList<WorkoutModel> = ArrayList()
+
+        val workout1 = WorkoutModel(R.drawable.chest_press, "chest press", "4 sets x  12-15 reps", "chest")
+        val workout2 = WorkoutModel(R.drawable.deadlift, "Deadlift", "4 sets x  10-12 reps", "full body")
+        // Add more items as needed
+
+
+        itemList.add(workout1)
+        itemList.add(workout2)
+
+        val adapter = WorkoutAdapter(itemList)
+        recyclerView.adapter = adapter
+
+
+        startButton = findViewById(R.id.startButton)
+        startButton.setOnClickListener {
+            recyclerView.visibility = View.VISIBLE
+        }
+    }
+}
---
+++ app/src/main/java/com/modarb/android/ui/onboarding/adapters/WorkoutAdapter.kt
@@ -0,0 +1,44 @@
+package com.modarb.android.ui.onboarding.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.modarb.android.R
+import com.modarb.android.ui.onboarding.models.WorkoutModel
+
+class WorkoutAdapter(private val itemList: List<WorkoutModel>) :
+    RecyclerView.Adapter<WorkoutAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var workoutImage: ImageView = itemView.findViewById(R.id.imageView6)
+        var workoutTitle: TextView = itemView.findViewById(R.id.textView9)
+        var workoutDescription: TextView = itemView.findViewById(R.id.textView11)
+        var workoutButton: Button = itemView.findViewById(R.id.button)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.workout_details, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = itemList[position]
+
+        holder.workoutImage.setImageResource(item.imageResId)
+        holder.workoutTitle.text = item.title
+        holder.workoutDescription.text = item.description
+        holder.workoutButton.text = item.button
+        holder.workoutButton.setOnClickListener {
+            // Handle button click
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+}
