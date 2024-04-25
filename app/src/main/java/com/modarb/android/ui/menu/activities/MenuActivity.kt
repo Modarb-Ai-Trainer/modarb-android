339352864626d9121e635527ba5002c23cdea210
Basem Khater <fagreleslam514@gmail.com>
2024-04-26 01:57:03 +0300

    Adding navigation to Reminders & Settings

--- app/src/main/java/com/modarb/android/ui/menu/activities/MenuActivity.kt
+++ app/src/main/java/com/modarb/android/ui/menu/activities/MenuActivity.kt
@@ -15,5 +15,13 @@ class MenuActivity : AppCompatActivity() {
         cardView.setOnClickListener {
             startActivity(Intent(this, ProfileActivity::class.java))
         }
+        val cardView2 = findViewById<View>(R.id.cardView5)
+        cardView2.setOnClickListener {
+            startActivity(Intent(this, ReminderActivity::class.java))
+        }
+        val cardView3 = findViewById<View>(R.id.cardView6)
+        cardView3.setOnClickListener {
+            startActivity(Intent(this, SettingActivity::class.java))
+        }
     }
 }
