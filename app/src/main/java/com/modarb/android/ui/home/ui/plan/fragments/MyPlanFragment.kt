382f4018c0de69e7068faa3ccef406b6fddadc28
Amr Hossam <amrhossam.dev@gmail.com>
2024-04-28 14:11:34 +0300

    Added view pager for my plan view.

--- app/src/main/java/com/modarb/android/ui/home/ui/plan/fragments/MyPlanFragment.kt
+++ app/src/main/java/com/modarb/android/ui/home/ui/plan/fragments/MyPlanFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.button.MaterialButtonToggleGroup
 import com.modarb.android.R
 import com.modarb.android.ui.workout.adapters.TrainingWeeksAdapter
@@ -16,44 +17,122 @@ import com.modarb.android.ui.workout.models.YourItem
 
 class MyPlanFragment : Fragment() {
 
-    private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: TrainingWeeksAdapter
-    private val dataList =
-        mutableListOf<YourItem>()
+    private lateinit var viewPager: ViewPager2
+    private lateinit var toggleButtonGroup: MaterialButtonToggleGroup
+    private val dataList = mutableListOf<YourItem>()
 
     @SuppressLint("NotifyDataSetChanged")
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
 
         val view = inflater.inflate(R.layout.fragment_my_plan, container, false)
 
-        recyclerView = view.findViewById(R.id.recyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(context)
-
-        adapter =
-            TrainingWeeksAdapter(dataList)
-        recyclerView.adapter = adapter
-
-        dataList.add(
-            YourItem(
-                "Week 1 : Foundation",
-                "Start easy in the first week to let your body get used to the workout. It sets the baseline for your progress in the weeks ahead."
-            )
-        )
-        dataList.add(
-            YourItem(
-                "Week 2 : Foundation",
-                "Start easy in the first week to let your body get used to the workout."
-            )
-        )
-
-        adapter.notifyDataSetChanged()
 
         val toggleGroup: MaterialButtonToggleGroup = view.findViewById(R.id.toggle_button_group)
         toggleGroup.check(R.id.myPlanBtn)
 
+        initViewPager(view)
+
         return view
+    }
+
+    private fun initViewPager(view: View) {
+        viewPager = view.findViewById(R.id.viewPager)
+        toggleButtonGroup = view.findViewById(R.id.toggle_button_group)
+
+        val adapter = ViewPagerAdapter()
+        viewPager.adapter = adapter
+
+        toggleButtonGroup.addOnButtonCheckedListener { _, checkedId, isChecked ->
+            if (isChecked) {
+                when (checkedId) {
+                    R.id.myPlanBtn -> viewPager.currentItem = 0
+                    R.id.customWorkOut -> viewPager.currentItem = 1
+                }
+            }
+        }
+
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                toggleButtonGroup.check(
+                    when (position) {
+                        0 -> R.id.myPlanBtn
+                        1 -> R.id.customWorkOut
+                        else -> View.NO_ID
+                    }
+                )
+            }
+        })
+    }
+
+    inner class ViewPagerAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            return when (viewType) {
+                0 -> MyPlanViewHolder(
+                    inflater.inflate(
+                        R.layout.my_plan_view, parent, false
+                    )
+                )
+
+                1 -> CustomWorkoutViewHolder(
+                    inflater.inflate(
+                        R.layout.custom_workout_view, parent, false
+                    )
+                )
+
+
+                else -> throw IllegalArgumentException("Invalid view type")
+            }
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            when (holder) {
+                is MyPlanViewHolder -> holder.bind()
+            }
+        }
+
+        override fun getItemCount(): Int = 2
+
+        override fun getItemViewType(position: Int): Int {
+            return position
+        }
+    }
+
+    inner class MyPlanViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var recyclerView: RecyclerView = view.findViewById(R.id.recyclerView)
+
+        fun bind() {
+            recyclerView.layoutManager = LinearLayoutManager(context)
+
+            adapter = TrainingWeeksAdapter(dataList)
+            recyclerView.adapter = adapter
+
+            dataList.add(
+                YourItem(
+                    "Week 1 : Foundation",
+                    "Start easy in the first week to let your body get used to the workout. It sets the baseline for your progress in the weeks ahead."
+                )
+            )
+            dataList.add(
+                YourItem(
+                    "Week 2 : Foundation",
+                    "Start easy in the first week to let your body get used to the workout."
+                )
+            )
+
+            adapter.notifyDataSetChanged()
+        }
+    }
+
+    inner class CustomWorkoutViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+//        private val textView: TextView = view.findViewById(R.id.textViewCustomWorkout)
+//
+//        fun bind(text: String) {
+//            textView.text = text
+//        }
     }
 }
