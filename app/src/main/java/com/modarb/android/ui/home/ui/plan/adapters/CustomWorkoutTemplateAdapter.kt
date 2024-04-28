4eac1d0f99f00a2b797bb4621771942bb810c1c9
Amr Hossam <amrhossam.dev@gmail.com>
2024-04-28 18:57:23 +0300

    Added custom workout adapter

---
+++ app/src/main/java/com/modarb/android/ui/home/ui/plan/adapters/CustomWorkoutTemplateAdapter.kt
@@ -0,0 +1,29 @@
+package com.modarb.android.ui.home.ui.plan.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.modarb.android.R
+
+class CustomWorkoutTemplateAdapter(
+    private val context: Context,
+) : RecyclerView.Adapter<CustomWorkoutTemplateAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context)
+            .inflate(R.layout.item_customworkout_template, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+        return 0
+    }
+}
