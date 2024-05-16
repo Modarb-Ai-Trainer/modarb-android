a071521b9919b8b177116eee674e38d7e20343ae
Amr Hossam <amrhossam.dev@gmail.com>
2024-05-16 21:46:10 +0300

    Refactor to view binding

--- app/src/main/java/com/modarb/android/ui/workout/adapters/DaysAdapter.kt
+++ app/src/main/java/com/modarb/android/ui/workout/adapters/DaysAdapter.kt
@@ -2,33 +2,29 @@ package com.modarb.android.ui.workout.adapters
 
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.modarb.android.R
+import com.modarb.android.databinding.ItemDaysLayoutBinding
 import com.modarb.android.ui.home.ui.plan.models.Day
-
 
 class DaysAdapter(private val itemList: List<Day>, private var isTheCurrentWeek: Boolean) :
     RecyclerView.Adapter<DaysAdapter.ItemViewHolder>() {
     private var isTheCurrentDayFound: Boolean = false
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.item_days_layout, parent, false)
-        return ItemViewHolder(view)
+        val binding =
+            ItemDaysLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ItemViewHolder(binding)
     }
 
-    class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val dayTitle: TextView = itemView.findViewById(R.id.dayText)
-    }
+    class ItemViewHolder(val binding: ItemDaysLayoutBinding) : RecyclerView.ViewHolder(binding.root)
 
     @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val dayData = itemList[position]
-        holder.dayTitle.text = "${position + 1}D"
+        holder.binding.dayText.text = "${position + 1}D"
 
         holder.itemView.background =
             ContextCompat.getDrawable(holder.itemView.context, R.drawable.item_day_style)
