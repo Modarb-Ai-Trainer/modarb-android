1ce3d5a612c92fa4535d30ab2bb1209615edf134
Amr Hossam <amrhossam.dev@gmail.com>
2024-05-16 21:42:25 +0300

    Refactor to view binding

--- app/src/main/java/com/modarb/android/ui/home/ui/plan/adapters/MyPlanViewPagerAdapter.kt
+++ app/src/main/java/com/modarb/android/ui/home/ui/plan/adapters/MyPlanViewPagerAdapter.kt
@@ -4,14 +4,13 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.modarb.android.R
+import com.modarb.android.databinding.CustomWorkoutViewBinding
+import com.modarb.android.databinding.MyPlanViewBinding
 import com.modarb.android.ui.home.ui.plan.logic.PlanViewModel
 import com.modarb.android.ui.home.ui.plan.models.Data
 import com.modarb.android.ui.home.ui.plan.models.Day
@@ -20,27 +19,17 @@ import com.modarb.android.ui.workout.adapters.TrainingWeeksAdapter
 
 class MyPlanViewPagerAdapter(
     private val context: Context, private var viewModel: PlanViewModel
-) :
-
-
-    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         return when (viewType) {
             0 -> MyPlanViewHolder(
-                inflater.inflate(
-                    R.layout.my_plan_view, parent, false
-                )
+                MyPlanViewBinding.inflate(inflater, parent, false)
             )
-
             1 -> CustomWorkoutViewHolder(
-                inflater.inflate(
-                    R.layout.custom_workout_view, parent, false
-                )
+                CustomWorkoutViewBinding.inflate(inflater, parent, false)
             )
-
-
             else -> throw IllegalArgumentException("Invalid view type")
         }
     }
@@ -49,7 +38,6 @@ class MyPlanViewPagerAdapter(
         when (holder) {
             is MyPlanViewHolder -> holder.bind(context)
             is CustomWorkoutViewHolder -> holder.bind(context)
-
         }
     }
 
@@ -59,31 +47,18 @@ class MyPlanViewPagerAdapter(
         return position
     }
 
-    inner class MyPlanViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        private var recyclerView: RecyclerView = view.findViewById(R.id.recyclerView)
-        private val fitLevel: TextView = view.findViewById(R.id.fitLevel)
-        private val fitGoal: TextView = view.findViewById(R.id.fitGoal)
-        private val planDesc: TextView = view.findViewById(R.id.planDesc)
-        private val workPlace: TextView = view.findViewById(R.id.workPlace)
-        private val workEquip: TextView = view.findViewById(R.id.workEquip)
-        private val workTime: TextView = view.findViewById(R.id.workTime)
-        private val workoutTotalDays: TextView = view.findViewById(R.id.workDays)
-        private val todayWorkoutTime: TextView = view.findViewById(R.id.todayWorkoutTime)
-        private val todayWorkoutName: TextView = view.findViewById(R.id.todayWorkoutNameTxt)
-        private val exerciseCount: TextView = view.findViewById(R.id.exerciseCount)
-        private val startWorkout: Button = view.findViewById(R.id.startWorkoutBtn)
+    inner class MyPlanViewHolder(private val binding: MyPlanViewBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         private lateinit var adapter: TrainingWeeksAdapter
 
-
         private fun setupRecyclerView(context: Context) {
-            recyclerView.layoutManager = LinearLayoutManager(context)
-
-
+            binding.recyclerView.layoutManager = LinearLayoutManager(context)
             if (!::adapter.isInitialized) {
                 adapter = TrainingWeeksAdapter(
-                    viewModel.planResponse.value?.body()?.data!!.weeks, context
+                    viewModel.planResponse.value?.body()?.data!!.weeks,
+                    context
                 )
-                recyclerView.adapter = adapter
+                binding.recyclerView.adapter = adapter
             }
         }
 
@@ -98,28 +73,24 @@ class MyPlanViewPagerAdapter(
 
         @SuppressLint("SetTextI18n")
         private fun updateTextViews(data: Data, context: Context) {
-            todayWorkoutName.text = getTodayWorkout()?.day_type
-            planDesc.text = data.workout.description
-            fitGoal.text = data.workout.fitness_goal
-            fitLevel.text = data.workout.fitness_level
-            workPlace.text = data.workout.place.joinToString(separator = ", ")
-            workTime.text = "${data.workout.min_per_day} ${context.getString(R.string.min)} / day"
-            todayWorkoutTime.text = "${data.workout.min_per_day} ${context.getString(R.string.min)}"
-            exerciseCount.text =
+            binding.todayWorkoutNameTxt.text = getTodayWorkout()?.day_type
+            binding.planDesc.text = data.workout.description
+            binding.fitGoal.text = data.workout.fitness_goal
+            binding.fitLevel.text = data.workout.fitness_level
+            binding.workPlace.text = data.workout.place.joinToString(separator = ", ")
+            binding.workTime.text =
+                "${data.workout.min_per_day} ${context.getString(R.string.min)} / day"
+            binding.todayWorkoutTime.text =
+                "${data.workout.min_per_day} ${context.getString(R.string.min)}"
+            binding.exerciseCount.text =
                 getTodayWorkout()?.total_number_exercises.toString() + " " + context.getString(R.string.exercise)
-            workEquip.text = data.workout.type
-            workoutTotalDays.text =
+            binding.workEquip.text = data.workout.type
+            binding.workDays.text =
                 data.weeks.size.toString() + " " + context.getString(R.string.days)
         }
 
-        /*
-    Function that will get today workout by finding the first is done = false week
-    then find the first undone exercise day
-
-     */
         private fun getTodayWorkout(): Day? {
             val weekList = viewModel.planResponse.value!!.body()!!.data.weeks
-
             for (week in weekList) {
                 if (!week.is_done) {
                     for (day in week.days) {
@@ -134,9 +105,8 @@ class MyPlanViewPagerAdapter(
         }
 
         private fun startWorkout() {
-            startWorkout.setOnClickListener {
+            binding.startWorkoutBtn.setOnClickListener {
                 context.startActivity(Intent(context, TodayWorkoutActivity::class.java))
-
             }
         }
 
@@ -145,32 +115,19 @@ class MyPlanViewPagerAdapter(
             loadDataIntoViews(context)
             startWorkout()
         }
-
-
     }
 
-    inner class CustomWorkoutViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        var recyclerView: RecyclerView = view.findViewById(R.id.recycleView)
-
+    inner class CustomWorkoutViewHolder(private val binding: CustomWorkoutViewBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         private val data = mutableListOf<String>()
 
         fun bind(context: Context) {
-            recyclerView.layoutManager = LinearLayoutManager(context)
-
-
+            binding.recycleView.layoutManager = LinearLayoutManager(context)
             for (i in 0..3) {
-                data.add(
-                    "test"
-                )
+                data.add("test")
             }
-
             val adapter = CustomWorkoutTemplateAdapter(context, data)
-            recyclerView.adapter = adapter
-
-
+            binding.recycleView.adapter = adapter
         }
     }
-
 }
-
-
