90717db378c40c51c2726e2f5e0929d59d6aaade
Amr Hossam <amrhossam.dev@gmail.com>
2024-06-17 00:46:26 +0300

    Populate exercise insights

--- app/src/main/java/com/modarb/android/ui/workout/activities/WorkoutInsightsActivity.kt
+++ app/src/main/java/com/modarb/android/ui/workout/activities/WorkoutInsightsActivity.kt
@@ -1,9 +1,11 @@
 package com.modarb.android.ui.workout.activities
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.modarb.android.databinding.ActivityWorkoutInsightsBinding
+import com.modarb.android.ui.helpers.WorkoutData
 import com.modarb.android.ui.home.HomeActivity
 
 class WorkoutInsightsActivity : AppCompatActivity() {
@@ -14,9 +16,22 @@ class WorkoutInsightsActivity : AppCompatActivity() {
         binding = ActivityWorkoutInsightsBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        initViews()
         handleOnFinish()
+    }
 
-        // TODO init adapter for post meals and design the item meal
+    @SuppressLint("SetTextI18n")
+    private fun initViews() {
+        binding.exerciseCount.text =
+            "${WorkoutData.getTodayWorkout()!!.exercises.size} \n Exercises"
+        var totalDuration = 0
+        WorkoutData.getTodayWorkout()!!.exercises.forEach {
+            totalDuration += it.duration
+        }
+        binding.dayDetails.text =
+            " Day" + WorkoutData.getTodayWorkout()!!.day_number + " " + WorkoutData.getTodayWorkout()!!.day_type
+        binding.timeTxt.text = "${totalDuration / 60} \n Minutes"
+
     }
 
 
